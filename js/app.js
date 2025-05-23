aed7beb5dc397572a271079de55df7849d5993a3
Chriistoffel <c.toffel9@gmail.com>
2025-05-23 20:09:46 +0700

    AA

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/AR-APP.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/wwwroot.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
